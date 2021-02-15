15fa914090b5e591307ba0f2d9c77d0eb702617a
Vivian Stanley <vivianstanley@Vivians-MacBook-Air.local>
2021-02-14 18:35:30 -0800

    commit tree f1c4c4f99f23adcfd3fa3571f5b9b65d1628f3c5

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 3) do
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "grade"
+    t.datetime "birthdate"
+  end
 
 end
